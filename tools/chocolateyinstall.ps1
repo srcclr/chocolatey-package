c9f877044831512455c071f39ac76d684386b4b4
Jason Yeo <jasonyeo88@gmail.com>
2019-03-18 17:30:55 +0800

    :computer: Add 64 bit support for windows

--- tools/chocolateyinstall.ps1
+++ tools/chocolateyinstall.ps1
@@ -2,20 +2,25 @@
 $ErrorActionPreference = 'Stop';
 
 $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$url        = "https://download.srcclr.com/srcclr-$Env:chocolateyPackageVersion-windows.zip"
+$url        = "https://download.srcclr.com/srcclr-3.2.4-windows.zip"
+$url64      = "https://download.srcclr.com/srcclr-$Env:chocolateyPackageVersion-windows.zip"
 
 $packageArgs = @{
-  packageName   = $env:ChocolateyPackageName
-  unzipLocation = $toolsDir
-  fileType      = 'EXE'
-  url           = $url
+  packageName    = $env:ChocolateyPackageName
+  unzipLocation  = $toolsDir
+  fileType       = 'EXE'
+  url            = $url
+  url64bit       = $url64
 
-  softwareName  = 'srcclr*'
+  softwareName   = 'srcclr*'
 
-  checksum      = '5ce02e0c1eb47661a4699d3ccdf44a171a95c84db74550f41ab1f2bdea9993fa'
-  checksumType  = 'sha256'
+  checksum       = '5ce02e0c1eb47661a4699d3ccdf44a171a95c84db74550f41ab1f2bdea9993fa'
+  checksumType   = 'sha256'
 
-  validExitCodes= @(0, 3010, 1641)
+  checksum64     = '5ce02e0c1eb47661a4699d3ccdf44a171a95c84db74550f41ab1f2bdea9993fa'
+  checksumType64 = 'sha256'
+
+  validExitCodes = @(0, 3010, 1641)
 }
 
 Install-ChocolateyZipPackage @packageArgs
