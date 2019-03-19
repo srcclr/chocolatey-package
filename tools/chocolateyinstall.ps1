d5891f03c7dcc75d6f385c3caad42f08f4d2e7d3
Jenkins User <nobody@srcclr.com>
2019-03-19 08:07:37 +0000

    Updated tools/chocolateyinstall.ps1 and srcclr.nuspec for version 3.2.6

--- tools/chocolateyinstall.ps1
+++ tools/chocolateyinstall.ps1
@@ -17,7 +17,7 @@ $packageArgs = @{
   checksum       = '5ce02e0c1eb47661a4699d3ccdf44a171a95c84db74550f41ab1f2bdea9993fa'
   checksumType   = 'sha256'
 
-  checksum64     = '9313c8c31abd67aede5fedf4a2eca219b7509f39f613d6eb7c1b0bc47b576138'
+  checksum64     = '26da522de7eea9b052e6d5262f4932559a6fd427d32867656140ec1366278aee'
   checksumType64 = 'sha256'
 
   validExitCodes = @(0, 3010, 1641)
